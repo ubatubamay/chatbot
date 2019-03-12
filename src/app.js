fe818e61063276cd0d5d6a214238e5a6e6543b94
Mayra <mayra@ubots.com.br>
2019-03-12 09:47:45 -0300

    Initialization

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './app.scss';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+ render() {
+   return (
+     <div>
+        <h1>MERRRRRN</h1>
+     </div>
+   );
+ }
+}
+
+export default App;
